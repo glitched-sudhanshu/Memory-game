a7c9ebdf8b65a216c51eee4a07fe4f6bb42739b5
sudhanshu <r02.sudhanshu@gmail.com>
2022-01-22 13:48:35 +0530

    Working on Recycler view adapter class

--- app/src/main/java/com/example/memorygame/MainActivity.kt
+++ app/src/main/java/com/example/memorygame/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.memorygame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.GridLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rvBoard: RecyclerView
+    private lateinit var txtMovesNum: TextView
+    private lateinit var txtPairsNum: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rvBoard = findViewById(R.id.rvBoard)
+        txtMovesNum = findViewById(R.id.txtMovesNum)
+        txtPairsNum = findViewById(R.id.txtPairsNum)
+
+        rvBoard.adapter = MemoryBoardAdapter(this, 8)
+        rvBoard.setHasFixedSize(true)
+        rvBoard.layoutManager = GridLayoutManager(this, 2);
     }
+
+
 }

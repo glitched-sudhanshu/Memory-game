5d57af2d697611d932cee76cf4a456ca9057afbd
sudhanshu <r02.sudhanshu@gmail.com>
2022-01-23 20:36:08 +0530

    Base game completed. Currently working on various menu items

--- app/src/main/java/com/example/memorygame/MainActivity.kt
+++ app/src/main/java/com/example/memorygame/MainActivity.kt
@@ -4,18 +4,19 @@ import android.animation.ArgbEvaluator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.GridLayout
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.memorygame.models.BoardSize
-import com.example.memorygame.models.MemoryCard
-import com.example.memorygame.utlis.DEFAULT_ICONS
 import com.example.memorygame.utlis.MemoryGame
 import com.google.android.material.snackbar.Snackbar
-import javax.security.auth.login.LoginException
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -42,6 +43,73 @@ class MainActivity : AppCompatActivity() {
         txtPairsNum = findViewById(R.id.txtPairsNum)
         clRoot = findViewById(R.id.clRoot)
 
+        setupBoard()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.mi_refresh -> {
+                if (memoryGame.numMoves() > 0 && !memoryGame.haveWonGame()) {
+                    showAlertDialog(
+                        "Do you really want to quit? You will loose your progress",
+                        null
+                    ) {
+                        setupBoard()
+                    }
+                } else
+                    setupBoard()
+                return true
+            }
+
+            R.id.mi_choose_size -> {
+                showNewSizeDialog()
+                return true
+            }
+
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun showNewSizeDialog() {
+        showAlertDialog("Choose new size", null, View.OnClickListener {
+            //set new value for board size
+        })
+    }
+
+    private fun showAlertDialog(title: String, view: View?, positiveClickListener: View.OnClickListener) {
+        AlertDialog.Builder(this)
+            .setTitle(title)
+            .setView(view)
+            .setNegativeButton("Cancel", null)
+            .setPositiveButton("OK"){
+                _,_->positiveClickListener.onClick(null)
+            }.show()
+
+    }
+
+    private fun setupBoard(){
+
+        when(boardSize){
+            BoardSize.EASY -> {
+                txtMovesNum.text = "Easy: 4 x 2"
+                txtPairsNum.text = "Pairs: 0 / 4"
+            }
+            BoardSize.MEDIUM ->  {
+                txtMovesNum.text = "Medium: 6 x 3"
+                txtPairsNum.text = "Pairs: 0 / 9"
+            }
+            BoardSize.HARD ->  {
+                txtMovesNum.text = "Hard: 6 x 4"
+                txtPairsNum.text = "Pairs: 0 / 12"
+            }
+        }
+
         txtPairsNum.setTextColor(ContextCompat.getColor(this, R.color.no_progress))
 
         memoryGame = MemoryGame(boardSize)
@@ -58,19 +126,20 @@ class MainActivity : AppCompatActivity() {
             })
         rvBoard.adapter = adapter
         rvBoard.setHasFixedSize(true)
-        rvBoard.layoutManager = GridLayoutManager(this, boardSize.getWidth());
+        rvBoard.layoutManager = GridLayoutManager(this, boardSize.getWidth())
     }
+
 
     private fun updateGameWithFlip(position: Int) {
         //Error checking
         if (memoryGame.haveWonGame()) {
             //alert the user of an invalid move
-            Snackbar.make(clRoot, "You already won!!", Snackbar.LENGTH_LONG).show()
+            Snackbar.make(clRoot, "You already won!", Snackbar.LENGTH_LONG).show()
             return
         }
         if (memoryGame.isCardFacedUp(position)) {
             //alert the user of an invalid move
-            Snackbar.make(clRoot, "Invalid won!!", Snackbar.LENGTH_SHORT).show()
+            Snackbar.make(clRoot, "Invalid move!", Snackbar.LENGTH_SHORT).show()
             return
         }
 
